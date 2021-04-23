9309919cef641dd39d82f33dbe17e621ba80dba2
Nilton Mendes <nilton.m.mendonca@gmail.com>
2021-04-23 18:32:51 -0300

    chore: Content and SideBar componentized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  selectedGenre: { title: string };
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Runtime: string;
+    Ratings: Array<{ Value: string }>;
+  }>;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
